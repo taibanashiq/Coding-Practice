6ecdbb88a3b9545c4bc858d24ed2b7f6175e1ab6
taibanashiq <taibanashiq@gmail.com>
2026-04-04 00:08:17 +0530

    add initial implementation of TwoSum algorithm

---
+++ TwoSum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>'
+int main(){
+    int arr[]={2,7,11,15};
+    int target=9;
+    for(int i=0;i<4;i++){
+        for(int j=i+1;j<4;j++){
+            if(arr[i]+arr[j]==target){
+                printf("[%d,%d]",i,j);
+            }
+        }
+    }
+}
