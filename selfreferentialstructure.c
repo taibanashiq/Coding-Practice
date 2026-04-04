07dce5a4ee58836a50703642aed66c51b7d90063
taibanashiq <taibanashiq@gmail.com>
2026-04-04 20:16:40 +0530

    add self-referential structure example

---
+++ selfreferentialstructure.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+int main()
+{
+    struct Node n1,n2;
+    scanf("%d",&n1.data);
+    scanf("%d",&n2.data);
+    n1.next=&n2;
+    n2.next=NULL;
+    printf("%d\n",n1.data);
+    printf("%d\n",n1.next->data);
+    return 0;
+
+}
