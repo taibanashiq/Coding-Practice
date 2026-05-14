d889b72985cf6368503323b17eb8d161612516cf
taibanashiq <taibanashiq@gmail.com>
2026-05-14 09:46:16 +0530

    Add binary tree implementation with preorder, postorder, and inorder traversal

---
+++ order.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<malloc.h>
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+struct node* createnode(int data){
+    struct node *n;
+    n=(struct node*)malloc(sizeof(struct node));
+    n->data=data;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+};
+void preorder(struct node* root){
+    if(root!=NULL){
+        printf("%d",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+void postorder(struct node* root){
+    if(root!=NULL){
+        postorder(root->left);
+    postorder(root->right);
+    printf("%d",root->data);
+            
+    }
+}
+void inorder(struct node* root){
+    if(root!=NULL){
+        inorder(root->left);
+         printf("%d",root->data);
+        inorder(root->right);
+            
+    }
+}
+int main(){
+    struct node *p=createnode(4);
+    struct node *p1=createnode(1);
+    struct node *p2=createnode(6);
+    struct node *p3=createnode(5);
+    struct node *p4=createnode(2);
+    p->right=p1;
+    p->left=p2;
+    p1->left=p3;
+    p1->right=p4;
+    preorder(p);
+    printf("\n");
+    postorder(p);
+    printf("\n");
+    inorder(p);
+    printf("\n");
+    return 0;
+}
+            
