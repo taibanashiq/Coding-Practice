b6cd34a177aa3cf3106e244054c318915a57816f
taibanashiq <taibanashiq@gmail.com>
2026-07-26 20:35:40 +0530

    print heloo world

---
+++ taibanashiqbutt/MessageProgram.java
@@ -0,0 +1,41 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         int n = 5;
+
+//         // Upper half
+//         for (int i = 0; i <= n; i++) {
+//             for (int j = 0; j <= i; j++)
+//                 System.out.print("*");
+
+//             int spaces = 2 * (n - i);
+//             for (int j = 0; j <= spaces; j++)
+//                 System.out.print(" ");
+
+//             for (int j = 0; j <= i; j++)
+//                 System.out.print("*");
+
+//             System.out.println();
+//         }
+
+//         // Lower half
+//         for (int i = n - 1; i >= 0; i--) {
+//             for (int j = 0; j <= i; j++)
+//                 System.out.print("*");
+
+//             int spaces = 2 * (n - i);
+//             for (int j = 0; j <= spaces; j++)
+//                 System.out.print(" ");
+
+//             for (int j = 0; j <= i; j++)
+//                 System.out.print("*");
+
+//             System.out.println();
+//         }
+//     }
+// }
+public class MessageProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to Java");
+    }
+}
+
---
+++ taibanashiqbutt/import numpy as np.py
@@ -0,0 +1,7 @@
+import numpy as np
+a=np.array([10,20,30])
+b=np.array([1,2,3])
+print("Addition:",a+b)
+print("subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
