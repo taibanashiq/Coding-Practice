5c4a62f5022251fef820d348b2ec3b9356344557
taibanashiq <taibanashiq@gmail.com>
2026-07-27 23:59:10 +0530

    input and outputs

--- Main.java/Main.java
+++ Main.java/Main.java
@@ -1,5 +1,13 @@
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner input=new Scanner(System.in);
+        // System.out.println("please enter some input:");
+        // int rollno=input.nextInt();
+        // System.out.println("your roll no is:"+rollno);
+    int marks=input.nextInt();
+    System.out.println("my marks are:"+marks);
+   
     }
 }
