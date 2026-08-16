bdeead4b4fa235d319553fae7062e38c40761aa5
taibanashiq <taibanashiq@gmail.com>
2026-08-16 22:55:46 +0530

    conditon and loops

---
+++ Main.java/mian.java
@@ -0,0 +1,13 @@
+class week5 {
+String colour="red";
+}
+class week4 extends week5{
+    String colour="blue";
+    void man(){
+        System.out.println("the colours");
+        System.out.println("the updation");
+    }
+}
+public class main{
+
+}
